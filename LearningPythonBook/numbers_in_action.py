f94259e37b7a283bce6168e260697b7f9fe87655
Adam Figgins <adam@pop-os.localdomain>
2023-12-13 20:05:15 +0000

    Python - Numbers in action

---
+++ LearningPythonBook/numbers_in_action.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+a, b = 3, 4
+print(a, b)
+
+print("a modulus 2: " + str(a % 2))
--- LearningPythonBook/scrapbook.py
+++ LearningPythonBook/scrapbook.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
+import decimal
+import math
 
-bob = dict(name='Bob Smith', age=42, pay=30000, job='dev')
+bob = dict(name="Bob Smith", age=42, pay=30000, job="dev")
 
-if not 'height' in bob:
-    print('missing height, adding')
-    bob['height'] = 100
+if "height" not in bob:
+    print("missing height, adding")
+    bob["height"] = 100
 
 # Sort the keys
 for key in sorted(bob):
-    print(key, '=>', bob[key])
+    print(key, "=>", bob[key])
+
+d = decimal.Decimal(math.pi, decimal.Context(prec=3))
+print(d)
